89d5c3a7e472353820af38ae2a108c2c420b4fa5
Sandeep Kambhampati <51316814+skambha6@users.noreply.github.com>
2022-05-27 22:50:37 -0700

    Update link_functions.jl

--- src/link_functions.jl
+++ src/link_functions.jl
@@ -18,8 +18,10 @@ function get_αβ_from_ϕ(ϕ::Vector{Float64}, rs::RegStruct)::NTuple{2,Vector{F
     # Obtain full α & β vectors
     a = rs.Nl[1]
     b = ϕ[1]
+    print("L: ")
     println(rs.L)
-    println(length(rs.pl))
+    print("pl: ")
+    println(length(rs.ρl))
     c = rs.ρl[1]
     
     α = [rs.Nl[l] * (ϕ[1] + ϕ[2] * rs.ρl[l]) for l = 1:rs.L]
